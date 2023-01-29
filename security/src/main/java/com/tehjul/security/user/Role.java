b59a7494d82568ac69b6ee99d41e57ed054b4d7c
tehjul <sibillej@gmail.com>
2023-01-29 12:02:29 +0100

    Add Spring security 3.0 course

---
+++ security/src/main/java/com/tehjul/security/user/Role.java
@@ -0,0 +1,5 @@
+package com.tehjul.security.user;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ security/src/test/java/com/tehjul/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tehjul.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
