a701d9170eacfe848fc1f911e10e42139560a9cb
tehjul <sibillej@gmail.com>
2023-02-01 17:39:32 +0100

    add exception handler

--- gestiondestock/src/main/java/com/tehjul/gestiondestock/handlers/ErrorDto.java
+++ gestiondestock/src/main/java/com/tehjul/gestiondestock/handlers/ErrorDto.java
@@ -1,2 +1,23 @@
-package com.tehjul.gestiondestock.handlers;public class ErrorDto {
+package com.tehjul.gestiondestock.handlers;
+
+import com.tehjul.gestiondestock.exception.ErrorCodes;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorDto {
+
+    private Integer httpCode;
+
+    private ErrorCodes code;
+
+    private String message;
+
+    private List<String> errors = new ArrayList<>();
 }
