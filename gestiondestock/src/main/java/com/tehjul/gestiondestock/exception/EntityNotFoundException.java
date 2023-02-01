68f17303a5d06578bcaec50daf4c1cb9a6b8e372
tehjul <sibillej@gmail.com>
2023-02-01 16:46:15 +0100

    add custom exceptions

--- gestiondestock/src/main/java/com/tehjul/gestiondestock/exception/EntityNotFoundException.java
+++ gestiondestock/src/main/java/com/tehjul/gestiondestock/exception/EntityNotFoundException.java
@@ -1,2 +1,24 @@
-package com.tehjul.gestiondestock.exception;public class EntityNotFoundException {
+package com.tehjul.gestiondestock.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    private ErrorCodes errorCode;
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause, ErrorCodes errorCode) {
+        super(message, cause);
+        this.errorCode = errorCode;
+    }
+
+    public EntityNotFoundException(String message, ErrorCodes errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
 }
