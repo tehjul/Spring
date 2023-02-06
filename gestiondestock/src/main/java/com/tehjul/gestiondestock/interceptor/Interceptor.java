4ef1989a008c43099358d59a3285d3c0856de5d0
tehjul <sibillej@gmail.com>
2023-02-06 23:57:38 +0100

    add interceptor

---
+++ gestiondestock/src/main/java/com/tehjul/gestiondestock/interceptor/Interceptor.java
@@ -0,0 +1,18 @@
+package com.tehjul.gestiondestock.interceptor;
+
+import org.hibernate.EmptyInterceptor;
+import org.springframework.util.StringUtils;
+
+public class Interceptor extends EmptyInterceptor {
+
+    @Override
+    public String onPrepareStatement(String sql) {
+        if (StringUtils.hasLength(sql) && sql.toLowerCase().startsWith("select")) {
+            if (sql.contains("where")) {
+            sql = sql + " and id_entreprise = 1";
+            }
+            sql = sql + " where id_entreprise = 1";
+        }
+        return super.onPrepareStatement(sql);
+    }
+}
--- gestiondestock/src/main/java/com/tehjul/gestiondestock/model/AbstractEntity.java
+++ gestiondestock/src/main/java/com/tehjul/gestiondestock/model/AbstractEntity.java
@@ -27,14 +27,4 @@ public class AbstractEntity implements Serializable {
     @Column(name = "lastModifiedDate")
     private Instant lastModifiedDate;
 
-    @PrePersist
-    void prePersist() {
-        creationDate = Instant.now();
-    }
-
-    @PreUpdate
-    void preUpdate() {
-        lastModifiedDate = Instant.now();
-    }
-
 }
