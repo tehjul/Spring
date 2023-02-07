dc1ad3694616c95e788dd9cc070750ed18371ff4
tehjul <sibillej@gmail.com>
2023-02-07 23:29:29 +0100

    add password encoder for Utilisateur

--- gestiondestock/src/main/java/com/tehjul/gestiondestock/services/impl/UtilisateurServiceImpl.java
+++ gestiondestock/src/main/java/com/tehjul/gestiondestock/services/impl/UtilisateurServiceImpl.java
@@ -4,14 +4,18 @@ import com.tehjul.gestiondestock.dto.UtilisateurDto;
 import com.tehjul.gestiondestock.exception.EntityNotFoundException;
 import com.tehjul.gestiondestock.exception.ErrorCodes;
 import com.tehjul.gestiondestock.exception.InvalidEntityException;
+import com.tehjul.gestiondestock.model.Utilisateur;
 import com.tehjul.gestiondestock.repository.UtilisateurRepository;
 import com.tehjul.gestiondestock.services.UtilisateurService;
 import com.tehjul.gestiondestock.validator.UtilisateurValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,10 +23,12 @@ import java.util.stream.Collectors;
 public class UtilisateurServiceImpl implements UtilisateurService {
 
     private final UtilisateurRepository utilisateurRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UtilisateurServiceImpl(UtilisateurRepository utilisateurRepository) {
+    public UtilisateurServiceImpl(UtilisateurRepository utilisateurRepository, PasswordEncoder passwordEncoder) {
         this.utilisateurRepository = utilisateurRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -33,11 +39,23 @@ public class UtilisateurServiceImpl implements UtilisateurService {
             throw new InvalidEntityException("L'utilisateur n'est pas valide", ErrorCodes.UTILISATEUR_NOT_VALID, errors);
         }
 
+        if (userAlreadyExists(dto.getEmail())) {
+            throw new InvalidEntityException("Un autre utilisateur avec le meme email existe deja", ErrorCodes.UTILISATEUR_ALREADY_EXISTS,
+                    Collections.singletonList("Un autre utilisateur avec le meme email existe deja dans la BDD"));
+        }
+
+        dto.setMotDePasse(passwordEncoder.encode(dto.getMotDePasse()));
+
         return UtilisateurDto.fromEntity(
                 utilisateurRepository.save(
                         UtilisateurDto.toEntity(dto)
                 )
         );
+    }
+
+    private boolean userAlreadyExists(String email) {
+        Optional<Utilisateur> user = utilisateurRepository.findUtilisateurByEmail(email);
+        return user.isPresent();
     }
 
     @Override
