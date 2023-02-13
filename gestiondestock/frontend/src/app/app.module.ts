74758e420fc136d2cb5428909835bea7cda4662f
tehjul <sibillej@gmail.com>
2023-02-13 16:01:28 +0100

    add routing

--- gestiondestock/frontend/src/app/app.module.ts
+++ gestiondestock/frontend/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
 import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
+import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
+import { PageStatistiquesComponent } from './pages/page-statistiques/page-statistiques.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageLoginComponent,
-    PageInscriptionComponent
+    PageInscriptionComponent,
+    PageDashboardComponent,
+    PageStatistiquesComponent
   ],
   imports: [
     BrowserModule,
