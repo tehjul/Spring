6159a398c403b1c4b5f7affb7abeff99c25cae1c
tehjul <tehjul.dev@gmail.com>
2023-02-22 23:11:57 +0100

    implementation of articles

--- gestiondestock/frontend/src/app/composants/detail-article/detail-article.component.ts
+++ gestiondestock/frontend/src/app/composants/detail-article/detail-article.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {ArticleDto} from "../../../gs-api/src/models/article-dto";
 
 @Component({
   selector: 'app-detail-article',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./detail-article.component.scss']
 })
 export class DetailArticleComponent {
+
+  @Input()
+  articleDto: ArticleDto = {};
 
 }
--- gestiondestock/frontend/src/app/pages/articles/page-article/page-article.component.ts
+++ gestiondestock/frontend/src/app/pages/articles/page-article/page-article.component.ts
@@ -1,17 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {ArticleDto} from "../../../../gs-api/src/models/article-dto";
+import {ArticleService} from "../../../services/article/article.service";
 
 @Component({
   selector: 'app-page-article',
   templateUrl: './page-article.component.html',
   styleUrls: ['./page-article.component.scss']
 })
-export class PageArticleComponent {
+export class PageArticleComponent implements OnInit {
+  listArticle: Array<ArticleDto> = [];
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private articleService: ArticleService
+  ) {
   }
 
   nouvelArticle(): void {
     this.router.navigate(['nouvelarticle']);
+  }
+
+  ngOnInit(): void {
+    this.articleService.findAllArticles()
+      .subscribe(articles => {
+        this.listArticle = articles;
+      })
   }
 }
