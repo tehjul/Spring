027c0c6aaec4bcf193309eb50ca400831d7767b4
tehjul <tehjul.dev@gmail.com>
2023-02-22 23:03:17 +0100

    implementation of nouvel-article

--- gestiondestock/frontend/src/app/pages/articles/nouvel-article/nouvel-article.component.ts
+++ gestiondestock/frontend/src/app/pages/articles/nouvel-article/nouvel-article.component.ts
@@ -1,20 +1,53 @@
-import {Component} from '@angular/core';
-import {Router} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {ArticleService} from "../../../services/article/article.service";
+import {ArticleDto} from "../../../../gs-api/src/models/article-dto";
+import {CategoryDto} from "../../../../gs-api/src/models/category-dto";
+import {CategoryService} from "../../../services/category/category.service";
 
 @Component({
   selector: 'app-nouvel-article',
   templateUrl: './nouvel-article.component.html',
   styleUrls: ['./nouvel-article.component.scss']
 })
-export class NouvelArticleComponent {
+export class NouvelArticleComponent implements OnInit {
 
+  articleDto: ArticleDto = {};
+  categorieDto: CategoryDto = {};
+  listeCategorie: Array<CategoryDto> = [];
+  errorMsg: Array<string> = [];
 
   constructor(
-    private router: Router
+    private router: Router,
+    private articleService: ArticleService,
+    private activatedRoute: ActivatedRoute,
+    private categoryService: CategoryService
   ) {
   }
 
+  ngOnInit(): void {
+    this.categoryService.findAllCategories()
+      .subscribe(categories => {
+        this.listeCategorie = categories;
+      })
+  }
+
+
   cancel(): void {
+    this.navigateToArticle();
+  }
+
+  enregistrerArticle() {
+    this.articleDto.category = this.categorieDto;
+    this.articleService.enregistrerArticle(this.articleDto)
+      .subscribe(art => {
+        this.navigateToArticle();
+      }, error => {
+        this.errorMsg = error.error.errors;
+      });
+  }
+
+  navigateToArticle(): void {
     this.router.navigate(['articles']);
   }
 }
---
+++ gestiondestock/frontend/src/app/services/article/article.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {UserService} from "../user/user.service";
+import {ArticlesService} from "../../../gs-api/src/services/articles.service";
+import {ArticleDto} from "../../../gs-api/src/models/article-dto";
+import {Observable, of} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+
+  constructor(
+    private userService: UserService,
+    private articleService: ArticlesService
+  ) {
+  }
+
+  enregistrerArticle(articleDto: ArticleDto): Observable<ArticleDto> {
+    articleDto.idEntreprise = this.userService.getConnectedUser().entreprise?.id;
+    return this.articleService.save(articleDto);
+  }
+
+  findAllArticles(): Observable<ArticleDto[]> {
+    return this.articleService.findAll();
+  }
+
+  findArticleById(id?: number): Observable<ArticleDto> {
+    if (id) {
+      return this.articleService.findById(id);
+    }
+    return of();
+  }
+
+}
