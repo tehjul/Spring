91e24cd1e6d1888db593a3c852ca66d695486bd0
tehjul <tehjul.dev@gmail.com>
2023-02-24 14:48:50 +0100

    implements add photo on article

--- gestiondestock/frontend/src/app/pages/articles/nouvel-article/nouvel-article.component.ts
+++ gestiondestock/frontend/src/app/pages/articles/nouvel-article/nouvel-article.component.ts
@@ -4,6 +4,8 @@ import {ArticleService} from "../../../services/article/article.service";
 import {ArticleDto} from "../../../../gs-api/src/models/article-dto";
 import {CategoryDto} from "../../../../gs-api/src/models/category-dto";
 import {CategoryService} from "../../../services/category/category.service";
+import {PhotosService} from "../../../../gs-api/src/services/photos.service";
+import SavePhotoParams = PhotosService.SavePhotoParams;
 
 @Component({
   selector: 'app-nouvel-article',
@@ -16,12 +18,15 @@ export class NouvelArticleComponent implements OnInit {
   categorieDto: CategoryDto = {};
   listeCategorie: Array<CategoryDto> = [];
   errorMsg: Array<string> = [];
+  file: File | null = null;
+  imgUrl: string | ArrayBuffer = 'favicon.ico';
 
   constructor(
     private router: Router,
     private articleService: ArticleService,
     private activatedRoute: ActivatedRoute,
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private photoService: PhotosService
   ) {
   }
 
@@ -50,6 +55,7 @@ export class NouvelArticleComponent implements OnInit {
     this.articleDto.category = this.categorieDto;
     this.articleService.enregistrerArticle(this.articleDto)
       .subscribe(art => {
+        this.savePhoto(art.id, art.codeArticle);
         this.navigateToArticle();
       }, error => {
         this.errorMsg = error.error.errors;
@@ -63,6 +69,39 @@ export class NouvelArticleComponent implements OnInit {
   calculerTTC(): void {
     if (this.articleDto.prixUnitaireHt && this.articleDto.tauxTva) {
       this.articleDto.prixUnitaireTtc = this.articleDto.prixUnitaireHt * (1 + this.articleDto.tauxTva / 100)
+    }
+  }
+
+
+  onFileInput(files: FileList | null): void {
+    if (files) {
+      this.file = files.item(0);
+      if (this.file) {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(this.file);
+        fileReader.onload = (event) => {
+          if (fileReader.result) {
+            this.imgUrl = fileReader.result;
+          }
+        }
+      }
+    }
+  }
+
+  savePhoto(idArticle?: number, titre?: string): void {
+    if (idArticle && titre && this.file) {
+      const params: SavePhotoParams = {
+        id: idArticle,
+        file: this.file,
+        title: titre,
+        context: 'article'
+      };
+      this.photoService.savePhoto(params)
+        .subscribe(res => {
+          this.navigateToArticle();
+        })
+    } else {
+      this.navigateToArticle();
     }
   }
 }
