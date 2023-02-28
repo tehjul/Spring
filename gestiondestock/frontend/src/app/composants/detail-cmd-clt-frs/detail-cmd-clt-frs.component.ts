e744a00d63a942de30206cc1d951946bef5d59e6
tehjul <tehjul.dev@gmail.com>
2023-02-28 16:36:01 +0100

    implementation of commandes frs clt

--- gestiondestock/frontend/src/app/composants/detail-cmd-clt-frs/detail-cmd-clt-frs.component.ts
+++ gestiondestock/frontend/src/app/composants/detail-cmd-clt-frs/detail-cmd-clt-frs.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {CommandeClientDto} from "../../../gs-api/src/models/commande-client-dto";
+import {ClientDto} from "../../../gs-api/src/models/client-dto";
+import {CmdcltfrsService} from "../../services/cmdcltfrs/cmdcltfrs.service";
+import {LigneCommandeClientDto} from "../../../gs-api/src/models/ligne-commande-client-dto";
+import {CommandeFournisseurDto} from "../../../gs-api/src/models/commande-fournisseur-dto";
 
 @Component({
   selector: 'app-detail-cmd-clt-frs',
   templateUrl: './detail-cmd-clt-frs.component.html',
   styleUrls: ['./detail-cmd-clt-frs.component.scss']
 })
-export class DetailCmdCltFrsComponent {
+export class DetailCmdCltFrsComponent implements OnInit{
+
+  @Input()
+  origin = '';
+
+  @Input()
+  commande: any = {};
+  cltFrs: ClientDto | undefined = {};
+
+  ngOnInit(): void {
+    this.extractClientFournisseur();
+  }
+
+  extractClientFournisseur(): void {
+    if (this.origin === 'client') {
+      this.cltFrs = this.commande?.client;
+    } else if (this.origin === 'fournisseur') {
+      this.cltFrs = this.commande?.fournisseur;
+    }
+  }
 
 }
--- gestiondestock/frontend/src/app/pages/page-cmd-clt-frs/page-cmd-clt-frs.component.ts
+++ gestiondestock/frontend/src/app/pages/page-cmd-clt-frs/page-cmd-clt-frs.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
+import {CmdcltfrsService} from "../../services/cmdcltfrs/cmdcltfrs.service";
+import {CommandeClientDto} from "../../../gs-api/src/models/commande-client-dto";
+import {LigneCommandeClientDto} from "../../../gs-api/src/models/ligne-commande-client-dto";
+import {LigneCommandeFournisseurDto} from "../../../gs-api/src/models/ligne-commande-fournisseur-dto";
 
 @Component({
   selector: 'app-page-cmd-clt-frs',
@@ -9,10 +13,23 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class PageCmdCltFrsComponent implements OnInit {
 
   origin = '';
+  listeCommandes: Array<any> = [];
+  mapLignesCommande = new Map();
+  mapPrixTotalCommande = new Map();
+  mapNbLignes = new Map();
+
   constructor(
     private router: Router,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private cmdCltFrsService: CmdcltfrsService
   ) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.data.subscribe(data => {
+      this.origin = data['origin'];
+    });
+    this.findAllCommandes();
   }
 
   nouvelleCommande(): void {
@@ -23,9 +40,70 @@ export class PageCmdCltFrsComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void {
-    this.activatedRoute.data.subscribe(data => {
-      this.origin = data['origin'];
-    });
+  findAllCommandes(): void {
+    if (this.origin === 'client') {
+      this.cmdCltFrsService.findAllCommandesClient()
+        .subscribe(cmd => {
+          this.listeCommandes = cmd;
+          this.findAllLigneCommande();
+        });
+    } else if (this.origin === 'fournisseur') {
+      this.cmdCltFrsService.findAllCommandesFournisseur()
+        .subscribe(cmd => {
+          this.listeCommandes = cmd;
+          this.findAllLigneCommande();
+        });
+    }
+  }
+
+  findAllLigneCommande(idCommande?: number): void {
+    this.listeCommandes.forEach(cmd => {
+      this.findLignesCommande(cmd.id);
+    })
+
+  }
+
+  private findLignesCommande(idCommande?: number): void {
+    if (this.origin === 'client') {
+      this.cmdCltFrsService.findAllLigneCommandesClient(idCommande)
+        .subscribe(list => {
+          this.fillMappings(idCommande, list);
+          // this.mapLignesCommande.set(idCommande, list);
+          // this.mapNbLignes.set(idCommande, list.length);
+          // this.mapPrixTotalCommande.set(idCommande, this.calculerTotalCmd(list));
+        });
+    } else if (this.origin === 'fournisseur') {
+      this.cmdCltFrsService.findAllLigneCommandesFournisseur(idCommande)
+        .subscribe(list => {
+          this.fillMappings(idCommande, list);
+          // this.mapLignesCommande.set(idCommande, list);
+          // this.mapNbLignes.set(idCommande, list.length);
+          // this.mapPrixTotalCommande.set(idCommande, this.calculerTotalCmd(list));
+        });
+    }
+  }
+
+  private fillMappings(idCmd: number | undefined, list: Array<LigneCommandeClientDto | LigneCommandeFournisseurDto>): void {
+    this.mapLignesCommande.set(idCmd, list);
+    this.mapNbLignes.set(idCmd, list.length);
+    this.mapPrixTotalCommande.set(idCmd, this.calculerTotalCmd(list));
+  }
+
+  calculerTotalCmd(list: Array<LigneCommandeClientDto>): number {
+    let total = 0;
+    list.forEach(ligne => {
+      if (ligne.quantite && ligne.prixUnitaire) {
+        total += ligne.quantite * ligne.prixUnitaire;
+      }
+    })
+    return +total.toFixed(2);
+  }
+
+  calculerTotalCommande(id?: number): number {
+    return this.mapPrixTotalCommande.get(id);
+  }
+
+  calculerNbLignes(id?: number): number {
+    return this.mapNbLignes.get(id);
   }
 }
